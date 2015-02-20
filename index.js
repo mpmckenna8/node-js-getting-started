4d70547b2cc20f0f211b6176a63a952690b077ae
Matthew McKenna <mpmckenna8@me.com>
2015-02-20 12:09:42 -0800

    adding a little space

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldnnnnooooo! made it!' + cool());
+  response.send('Hello Worldnnnnooooo! made it!   ' + cool());
 });
 
 app.listen(app.get('port'), function() {
