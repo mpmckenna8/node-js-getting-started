10aba6a63164c55ca3715a314a902e49f57e86d1
Matthew McKenna <mpmckenna8@me.com>
2015-02-21 15:32:19 -0800

    think I got the map working here

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var app = express();
 var cool = require("cool-ascii-faces");
+var pg = require("pg");
 
 app.set('port', (process.env.PORT || 5000));
+
+// app.set('views')
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldnnnnooooo! made it!   ' + cool());
+  response.sendFile(__dirname+"/index.html");
+  console.log(process.env);
 });
+
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+      { console.error(err); response.send("Error " + err); }
+      else
+      { response.send(result.rows); }
+    });
+  });
+})
+
+
+app.get('/calAss1.json', function(req, res){
+  res.sendFile(__dirname + "/calAss1.json");
+})
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
